c5a0acd72959c3be80fde8bc41977c572e29f3cc
kinchana123 <russia1973pun0suy@hotmail.com>
2024-05-09 04:28:46 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-function solveSudoku(board) {
-  solve(board);
-  function solve(board) {
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        if (board[i][j] === ".") {
-          for (let num = 1; num <= 9; num++) {
-            const numChar = num.toString();
-            if (isValid(board, i, j, numChar)) {
-              board[i][j] = numChar;
-              if (solve(board)) return true;
-              board[i][j] = ".";
-            }
-          }
-          return false;
-        }
-      }
-    }
-    return true;
+function lengthOfLongestSubstring(s) {
+  if (s.length === 0) return 0;
+  const map = new Map();
+  let maxLength = 0;
+  let left = 0;
+  for (let right = 0; right < s.length; right++) {
+    if (map.has(s[right])) left = Math.max(left, map.get(s[right]) + 1);
+    map.set(s[right], right);
+    maxLength = Math.max(maxLength, right - left + 1);
   }
-  function isValid(board, row, col, num) {
-    const boxRow = Math.floor(row / 3) * 3;
-    const boxCol = Math.floor(col / 3) * 3;
-    for (let i = 0; i < 9; i++) {
-      if (
-        board[row][i] === num ||
-        board[i][col] === num ||
-        board[boxRow + Math.floor(i / 3)][boxCol + (i % 3)] === num
-      )
-        return false;
-    }
-    return true;
-  }
+  return maxLength;
 }
