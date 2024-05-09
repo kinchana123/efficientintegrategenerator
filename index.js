37c2a68c6c22ece800ab0bfa093b61ac304e458b
kinchana123 <russia1973pun0suy@hotmail.com>
2024-05-09 09:47:27 +0700

    add more features

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-function lengthOfLongestSubstring(s) {
-  if (s.length === 0) return 0;
-  const map = new Map();
-  let maxLength = 0;
-  let left = 0;
-  for (let right = 0; right < s.length; right++) {
-    if (map.has(s[right])) left = Math.max(left, map.get(s[right]) + 1);
-    map.set(s[right], right);
-    maxLength = Math.max(maxLength, right - left + 1);
-  }
-  return maxLength;
+function minStack() {
+  this.stack = [];
+  this.minStack = [];
 }
+minStack.prototype.push = function (x) {
+  this.stack.push(x);
+  if (
+    this.minStack.length === 0 ||
+    x <= this.minStack[this.minStack.length - 1]
+  )
+    this.minStack.push(x);
+};
+minStack.prototype.pop = function () {
+  if (this.stack.pop() === this.minStack[this.minStack.length - 1])
+    this.minStack.pop();
+};
+minStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+minStack.prototype.getMin = function () {
+  return this.minStack[this.minStack.length - 1];
+};
