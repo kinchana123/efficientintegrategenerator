99fbb459eb3b4d57b401a69b22b3c99e2f12e6fe
kinchana123 <russia1973pun0suy@hotmail.com>
2024-05-10 13:49:58 +0700

    test project commit

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-function minStack() {
-  this.stack = [];
-  this.minStack = [];
+function isPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
 }
-minStack.prototype.push = function (x) {
-  this.stack.push(x);
-  if (
-    this.minStack.length === 0 ||
-    x <= this.minStack[this.minStack.length - 1]
-  )
-    this.minStack.push(x);
-};
-minStack.prototype.pop = function () {
-  if (this.stack.pop() === this.minStack[this.minStack.length - 1])
-    this.minStack.pop();
-};
-minStack.prototype.top = function () {
-  return this.stack[this.stack.length - 1];
-};
-minStack.prototype.getMin = function () {
-  return this.minStack[this.minStack.length - 1];
-};
+
+console.log(isPalindrome("racecar")); // true
