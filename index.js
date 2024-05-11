a5a92804254cba7bc42381133b0a74ac2584d2e9
kinchana123 <russia1973pun0suy@hotmail.com>
2024-05-12 01:27:07 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function isPalindrome(str) {
-  const reversed = str.split("").reverse().join("");
-  return str === reversed;
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+  if (n % 2 === 0 || n % 3 === 0) return false;
+  let i = 5;
+  while (i * i <= n) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+    i += 6;
+  }
+  return true;
 }
-
-console.log(isPalindrome("racecar")); // true
