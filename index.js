12e59f586b8b66e09d59d68ffd63bf877c18e66c
kinchana123 <russia1973pun0suy@hotmail.com>
2024-05-13 14:16:02 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function lengthOfLIS(nums) {
-  if (nums.length === 0) return 0;
-  const dp = new Array(nums.length).fill(1);
+function maxEnvelopes(envelopes) {
+  envelopes.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
+  const dp = new Array(envelopes.length).fill(1);
   let max = 1;
-  for (let i = 1; i < nums.length; i++) {
+  for (let i = 1; i < envelopes.length; i++) {
     for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) {
+      if (envelopes[i][1] > envelopes[j][1]) {
         dp[i] = Math.max(dp[i], dp[j] + 1);
         max = Math.max(max, dp[i]);
       }
