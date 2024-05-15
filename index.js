ddd81c9181fc2ff7196e290a182a62491175faba
kinchana123 <russia1973pun0suy@hotmail.com>
2024-05-15 12:51:28 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-function findItinerary(tickets) {
-  const graph = new Map();
-  for (const [from, to] of tickets) {
-    if (!graph.has(from)) graph.set(from, []);
-    graph.get(from).push(to);
-  }
-  for (const destinations of graph.values()) {
-    destinations.sort();
-  }
-  const result = [];
-  dfs("JFK");
-  return result.reverse();
-  function dfs(from) {
-    const destinations = graph.get(from);
-    while (destinations && destinations.length) {
-      dfs(destinations.shift());
-    }
-    result.push(from);
-  }
+function maxDepth(root) {
+  if (!root) return 0;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 }
